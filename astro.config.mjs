e69b998877fc02d5c546e5540a57d7d8e69105f4
Chamogelastos <stavros.ssta@gmail.com>
2026-06-18 19:27:35 +0300

    Get me out of this hell

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,55 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import tailwindcss from '@tailwindcss/vite';
+import starlightSidebarTopics from 'starlight-sidebar-topics'
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
+    site: 'https://docs.inertiaorg.net',
+  integrations: [
+      starlight({
+          plugins: [
+              starlightSidebarTopics([
+                  {
+                      label: 'Inertia',
+                      link: '/inertia/development/introduction',
+                      icon: 'open-book',
+                      items: ['inertia/development/introduction'],
+                  },
+                  {
+                      label: 'Our Story',
+                      link: '/ourstory/spigotforumpage',
+                      icon: 'information',
+                      items: ['ourstory/spigotforumpage'],
+                  },
+              ]),
+
+          ],
+          title: 'Inertia Documentation',
+          components: {
+              Footer: './src/components/overrides/Footer.astro',
+              Banner: './src/components/overrides/Banner.astro',
+              Sidebar: './src/components/Sidebar.astro',
+          },
+          editLink: {
+              baseUrl: 'https://github.com/InertiaOrg/starlight-docs/tree/master/',
+          },
+          tableOfContents: { minHeadingLevel: 1, maxHeadingLevel: 3 },
+          customCss: [
+              // Path to your Tailwind base styles:
+              './src/styles/global.css',
+              './src/fonts/font-face.css',
+          ],
+          logo: {
+          src: './src/assets/houston.webp',
+          },
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/InertiaOrg/starlight-docs' }],
+
+      }),
 	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
